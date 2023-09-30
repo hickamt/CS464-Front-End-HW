be27926cd77f727057e0c1d3d811e203481f8c62
Todd <thickam@pdx.edu>
2023-09-29 23:20:56 -0700

    finished exercise 2 & styled html page

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,3 +1,16 @@
-const elem = document.querySelector('input');
+function writeInnerHTML(innerValue) {
+  document.getElementById("palindrome").innerHTML = innerValue;
+}
 
-elem.addEventListener('input', handleInput);
+const isPalindrome = (fwd_array, rev_array) => {
+  return fwd_array.length === rev_array.length && fwd_array.length > 0
+    ? fwd_array.every((value, index) => value === rev_array[index])
+    : "No Value Entered";
+};
+
+document.querySelector("input").addEventListener("input", () => {
+  const inputValue = document.querySelector("input").value;
+  writeInnerHTML(
+    isPalindrome(inputValue.split(""), inputValue.split("").reverse())
+  );
+});
