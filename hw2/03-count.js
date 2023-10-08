7083e607324af20e3873c026c763337bfb05ae01
Todd <thickam@outlook.com>
2023-10-08 16:38:38 -0700

    quick save

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -1,3 +1,23 @@
-// Add your code here
+/* 
+  Bootstrap 5: Typography <mark></mark> to highlight word(s)
+*/
 
-input.addEventListener('keydown', handleKeyDown);
+const validateInput = function validateInputValue(inputValue) {
+  const regex = /[^a-z,^A-Z,^\.,^\,^\ \,]/gm;
+  const invalidArray = inputValue.matchAll(regex);
+  if (invalidArray) {
+    console.log(invalidArray)
+    // alert("Not a valid input, plase enter a word to highlight");
+  }
+};
+
+document.getElementById('form-input').addEventListener("keydown", (event) => {
+  const inputValue = "";
+  if (event.target.value.checkValidity()) {
+    inputValue = event.target.value;
+  }
+  else {
+    alert("Not a Valid Input")
+  }
+  // validateInput(inputValue)
+});
