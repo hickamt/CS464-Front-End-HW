2a37c4fce5237cd6f0b9204e799ddd35e4d76039
Todd <thickam@outlook.com>
2023-10-09 19:50:30 -0700

    added input validation and user feedback message for invalid input

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -1,25 +1,39 @@
 /* 
   Bootstrap 5: Typography <mark></mark> to highlight word(s)
-  Source of R.D. https://www.youtube.com/watch?v=Ixt6k9aatPQ
 */
 
-const validate = function validateInput(inputValue) {
-  // /[^a-z^A-Z^\/.\^\/,]/gmi
-}
-
-const escapeInput = function escapeInputValue(findInputValue) {
-  return findInputValue.replace(/[.*+?^${}()|[/]\\]/g, "\\$&");
+const m_error = {
+  message: "Invalid input, please enter a word to highlight",
 };
 
-document.getElementById("form-input").addEventListener("keydown", (event) => {
+/**
+ * Validate the user input is of characters a-z, A-Z, '.', or ',' only.
+ * @param inputValue is the form field input string
+ * @returns the inputValue if input string is valid
+ * If the input is invalid: user feedback becomes 'visible' and
+ * a new error() message is thrown
+ */
+const validate = function validateInput(inputValue) {
+  if (/[^a-z^A-Z^\/.\^\/,]/gim.test(inputValue)) {
+    document.getElementById("input-feedback").style.visibility = "visible";
+    throw new Error(m_error.message);
+  }
+  return inputValue;
+};
+
+const pipe =
+  (...functions) =>
+  (x) =>
+    functions.reduce((v, f) => f(v), x);
+
+addEventListener("keydown", (event) => {
   try {
-    const paragraph = document.getElementById("paragraph");
-    const findInputValue = event.target.value;
-    const highlight = escapeInput(findInputValue);
-    let foundRegEx = new RegExp(`${highlight}`, "gi");
-    paragraph.innerHTML = paragraph.textContent.replace(
-      foundRegEx,
-      (text) => `<mark>${text}</mark>`
-    );
-  } catch (error) {}
+    document.getElementById("input-feedback").style.visibility = "hidden";
+    setTimeout(() => {
+      const textInput = event.target.value;
+      validate(textInput);
+    }, 10);
+  } catch (error) {
+    console.error(error);
+  }
 });
