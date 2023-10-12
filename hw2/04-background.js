71e123ddcb1da228dc87b006061916bb15bf072c
Todd <thickam@outlook.com>
2023-10-11 21:52:13 -0700

    created random color generator and start event on 'load'

--- hw2/04-background.js
+++ hw2/04-background.js
@@ -6,23 +6,46 @@
  * Conversion: 1 second === 1000 milliseconds
  */
 
-const startButton = function timeStartButton() {
-  document.getElementById("start-stop-btn-container").innerHTML;
+/**
+ * Generate a pseudo random value within a range [min, max]
+ * @param min value for lower range (inclusive)
+ * @param max value for upper range (inclusive)
+ * @returns random value based on given [min, max] range
+ * @source MDN Math/random
+ */
+const getRandomValue = function getRandomValueUsingInclusiveRange(min, max) {
+  ceiling_min = Math.ceil(min);
+  floor_max = Math.floor(max);
+  return Math.floor(Math.random() * (floor_max - ceiling_min) + ceiling_min);
 };
 
-const toggleButton = function switchStartStopButton() {};
+/**
+ * Generates a red,green,blue,alpha() css style color using
+ * random value generator
+ * @param min value for lower range (inclusive)
+ * @param max value for upper range (inclusive)
+ * @returns rgba(val, val, val, 0.1)
+ */
+const getRGBA = function getRandomGeneratedRgbaColor(min = 0, max = 175) {
+  return `rgba(
+    ${getRandomValue(min, max)},
+    ${getRandomValue(min, max)},
+    ${getRandomValue(min, max)},
+    0.8)`;
+};
 
-addEventListener("click", (event) => {
-  let defaultTime = 3000; // 3 seconds
-  let changeColor = true;
-  try {
-    while (changeColor) {
-      document.getElementById("body").style.backgroundColor = "blue";
-      setTimeout(() => {
-        document.getElementById("body").style.backgroundColor = "black";
-      }, defaultTime);
-    }
-  } catch (error) {
-    console.error(error);
-  }
+/**
+ * Get random rgba() color and set to 'body' background color
+ */
+const changeColor = function changeBodyBackgroundColor() {
+  document.getElementById("body").style.backgroundColor = getRGBA(0, 175);
+};
+
+addEventListener("load", (event) => {
+  let defaultInterval = 3000; // 3 seconds
+
+  setInterval(() => {
+    changeColor(); 
+  }, defaultInterval);
+
 });
