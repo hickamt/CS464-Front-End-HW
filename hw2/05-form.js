64129db571cecdf742d18259fdada482635d557c
Todd <thickam@outlook.com>
2023-10-13 16:41:31 -0700

    added js event listener to console data from form field values

--- hw2/05-form.js
+++ hw2/05-form.js
@@ -1,5 +1,9 @@
-// Add your code here
+
+const form = document.getElementById('registration-form');
 
 form.addEventListener('submit', (event) => {
-  
+  event.preventDefault();
+  const formData = new FormData(event.target)
+  const formValues = Object.fromEntries(formData.entries())
+  console.log(formValues)
 })
