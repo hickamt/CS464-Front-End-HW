9a0a6b34a0f8703835f5a4d4d43ee7a774710691
Todd <thickam@outlook.com>
2023-10-13 21:05:31 -0700

    console log of each form value before creating the new page view

--- hw2/05-form.js
+++ hw2/05-form.js
@@ -1,9 +1,34 @@
+/**
+ * get form id 'registration-form' and set
+ * innerHTML = new body showing all values submitted
+ * form[0].reset();
+ */
 
-const form = document.getElementById('registration-form');
+// const form = document.getElementById('registration-form');
 
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const formData = new FormData(event.target)
-  const formValues = Object.fromEntries(formData.entries())
-  console.log(formValues)
-})
+/**
+ * Full Name: (name)
+ * Email: (email)
+ * Registration Status:
+ * Have you taken any of the following courses?
+ * (program-language, operating-systems, full-stack-web-dev)
+ * Comments:
+ */
+
+document
+  .getElementById("registration-form")
+  .addEventListener("submit", (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const formValues = Object.fromEntries(formData.entries());
+    console.log("Name: ", formValues.name);
+    console.log("Email: ", formValues.email);
+    console.log("Registration: ", formValues.registration);
+
+    const courses = document.querySelectorAll("input");
+    for (let course of courses) {
+      if (course.checked) {
+        console.log(course.value);
+      }
+    }
+  });
