220b4d01655d73daba055a234576866f59ebd152
Todd <thickam@outlook.com>
2023-10-25 16:25:01 -0700

    created fetch->filter->renderChart and linked 02-styles.css file to 02-charts.html

--- hw3/02-charts.js
+++ hw3/02-charts.js
@@ -1,45 +1,47 @@
 const backgroundColors = [
-  'rgba(54, 162, 235, 0.8)',
-  'rgba(255, 206, 86, 0.8)',
-  'rgba(255, 99, 132, 0.8)',
-  'rgba(75, 192, 192, 0.8)',
-  'rgba(153, 102, 255, 0.8)',
-  'rgba(255, 159, 64, 0.8)',
-  'rgba(199, 199, 199, 0.8)',
-  'rgba(83, 102, 255, 0.8)',
-  'rgba(40, 159, 64, 0.8)',
-  'rgba(210, 199, 199, 0.8)',
-  'rgba(78, 52, 199, 0.8)',
+  "rgba(54, 162, 235, 0.8)",
+  "rgba(255, 206, 86, 0.8)",
+  "rgba(255, 99, 132, 0.8)",
+  "rgba(75, 192, 192, 0.8)",
+  "rgba(153, 102, 255, 0.8)",
+  "rgba(255, 159, 64, 0.8)",
+  "rgba(199, 199, 199, 0.8)",
+  "rgba(83, 102, 255, 0.8)",
+  "rgba(40, 159, 64, 0.8)",
+  "rgba(210, 199, 199, 0.8)",
+  "rgba(78, 52, 199, 0.8)",
 ];
 
 const borderColors = [
-  'rgba(54, 162, 235, 1)',
-  'rgba(255, 206, 86, 1)',
-  'rgba(255, 99, 132, 1)',
-  'rgba(75, 192, 192, 1)',
-  'rgba(153, 102, 255, 1)',
-  'rgba(255, 159, 64, 1)',
-  'rgba(159, 159, 159, 1)',
-  'rgba(83, 102, 255, 1)',
-  'rgba(40, 159, 64, 1)',
-  'rgba(210, 199, 199, 1)',
-  'rgba(78, 52, 199, 1)',
+  "rgba(54, 162, 235, 1)",
+  "rgba(255, 206, 86, 1)",
+  "rgba(255, 99, 132, 1)",
+  "rgba(75, 192, 192, 1)",
+  "rgba(153, 102, 255, 1)",
+  "rgba(255, 159, 64, 1)",
+  "rgba(159, 159, 159, 1)",
+  "rgba(83, 102, 255, 1)",
+  "rgba(40, 159, 64, 1)",
+  "rgba(210, 199, 199, 1)",
+  "rgba(78, 52, 199, 1)",
 ];
 
-// url for the Thrones API
-const url = 'https://thronesapi.com/api/v2/Characters';
-
-const renderChart = () => {
-  const donutChart = document.querySelector('.donut-chart');
+/**
+ * Renders the chart data 'nameAndcount' as a doughnut chart
+ * @param nameAndCount contains the family name and length of name
+ */
+const renderChart = (nameAndCount) => {
+  const donutChart = document.querySelector(".donut-chart");
 
   new Chart(donutChart, {
-    type: 'doughnut',
+    type: "doughnut",
     data: {
-      labels: ['label', 'label', 'label', 'label'],
+      // labels will be an array of family names
+      labels: nameAndCount.map((name) => name.familyName),
       datasets: [
         {
-          label: 'My First Dataset',
-          data: [1, 12, 33, 5],
+          label: "Family Name & Length",
+          data: nameAndCount.map((name) => name.count),
           backgroundColor: backgroundColors,
           borderColor: borderColors,
           borderWidth: 1,
@@ -49,4 +51,70 @@ const renderChart = () => {
   });
 };
 
-renderChart();
+const filterRepeatedNames = (data) => {
+  const uniqueNames = [];
+  data.map((character) => {
+    const familyName = character.family.split("House").join("").trim();
+    if (!uniqueNames.includes(familyName)) {
+      uniqueNames.push(familyName);
+    }
+  });
+  return uniqueNames;
+};
+
+/**
+ * Seperates the family name from data
+ * Sets family name and length of name to new data array
+ * @param data array of charcter objects
+ * @returns nameAndCount array with family name and length of name
+ */
+const getChartData = function setFamilyNameAndLengthOfName(data) {
+  const uniqueNames = [];
+
+  if (data) {
+    data.map((character) => {
+      const familyName = character.family.split("House").join("").trim();
+      if (!uniqueNames.includes(familyName)) {
+        uniqueNames.push({
+          familyName: familyName,
+          count: familyName.length,
+        });
+      }
+    });
+  }
+  return uniqueNames;
+};
+
+/**
+ * Fetches data from the API
+ * @param url the url path required to fetch the data
+ * @returns the response data
+ */
+const fetchData = async function fetchCharactersAPI(url) {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw new Error("Error fetching data");
+  }
+};
+
+/**
+ * Event listener for the window on load event
+ * calling:
+ * fetchData()
+ * getChartData()
+ * renderChart()
+ */
+window.addEventListener("load", async () => {
+  // url for the Thrones API
+  const url = "https://thronesapi.com/api/v2/Characters";
+  try {
+    const data = await fetchData(url);
+    const nameAndCount = getChartData(data);
+    renderChart(nameAndCount);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
