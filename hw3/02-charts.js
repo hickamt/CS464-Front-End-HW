e281344b10911937d588bb03588ac07eb4dfb29b
Todd <thickam@outlook.com>
2023-10-25 20:42:17 -0700

    changed program to count the number of character occurances for each house, abstracted functionality and updated notes

--- hw3/02-charts.js
+++ hw3/02-charts.js
@@ -18,12 +18,16 @@ const getRandomValue = function getRandomValueUsingInclusiveRange(min, max) {
  * @param max value for upper range (inclusive)
  * @returns rgba(val, val, val, 0.1)
  */
-const getRGBA = function getRandomGeneratedRgbaColor(min = 0, max = 255) {
+const getRGBA = function getRandomGeneratedRgbaColor(
+  min = 0,
+  max = 255,
+  alpha = 0.5
+) {
   return `rgba(
     ${getRandomValue(min, getRandomValue(100, max))},
     ${getRandomValue(min, getRandomValue(100, max))},
     ${getRandomValue(min, getRandomValue(125, max))},
-    0.8)`;
+    ${alpha})`;
 };
 
 const backgroundColors = [
@@ -53,36 +57,36 @@ const backgroundColors = [
 
 const borderColors = [
   "rgba(54, 162, 235, 1)",
-  getRGBA(),
+  getRGBA(1),
   "rgba(255, 206, 86, 1)",
-  getRGBA(),
+  getRGBA(1),
   "rgba(255, 99, 132, 1)",
-  getRGBA(),
+  getRGBA(1),
   "rgba(75, 192, 192, 1)",
-  getRGBA(),
+  getRGBA(1),
   "rgba(153, 102, 255, 1)",
-  getRGBA(),
+  getRGBA(1),
   "rgba(255, 159, 64, 1)",
-  getRGBA(),
+  getRGBA(1),
   "rgba(159, 159, 159, 1)",
-  getRGBA(),
+  getRGBA(1),
   "rgba(83, 102, 255, 1)",
-  getRGBA(),
+  getRGBA(1),
   "rgba(40, 159, 64, 1)",
-  getRGBA(),
+  getRGBA(1),
   "rgba(210, 199, 199, 1)",
-  getRGBA(),
+  getRGBA(1),
   "rgba(78, 52, 199, 1)",
-  getRGBA(),
+  getRGBA(1),
 ];
 
 /**
- * Renders the chart data 'nameAndcount' as a doughnut chart
+ * Renders a doughnut chart for each character house name
+ * categorized by the number of house name occurances
  * @param familyNameAndCount contains the family name and length of name
  */
 const renderChart = (familyNameAndCount) => {
   const donutChart = document.querySelector(".donut-chart");
-
   new Chart(donutChart, {
     type: "doughnut",
     data: {
@@ -102,8 +106,49 @@ const renderChart = (familyNameAndCount) => {
       legend: {
         position: "bottom",
       },
+      title: {
+        display: true,
+        text: "Game of Thrones | Number of Characters in Each Family",
+        fontSize: 24,
+        fontWeight: "bold",
+      },
     },
   });
+};
+
+/**
+ * Function will count the number of times a family name occurs
+ * @param uniqueFamilyNames is an array of unique family names
+ * @param familyNamesOccurance is an array to set the count of each family name occurance
+ * Sets the count of each family name occurance to the familyNamesOccurance array
+ */
+const countFamilyNameOccurance = function setTheCountOfFamilyNameOccurances(
+  uniqueFamilyNames,
+  familyNames,
+  familyNamesOccurance
+) {
+  uniqueFamilyNames.map((name) => {
+    let count = 0;
+    familyNames.map((familyName) => {
+      if (name === familyName) {
+        count++;
+      }
+    });
+    familyNamesOccurance.push(count);
+  });
+};
+
+/**
+ * Function will combine the family names into a new array
+ * creating an array of unique family names
+ * @param familyNames is an array of all family names from the data API
+ * @returns a unique array of family names
+ */
+const combineFamilyNames = function combineUniqueFamilyNames(familyNames) {
+  let combinedNames = familyNames.filter((name, index) => {
+    return familyNames.indexOf(name) === index;
+  });
+  return combinedNames;
 };
 
 /**
@@ -128,11 +173,11 @@ const sortAlpha = function sortDataAlphabeticallyAtoZ(data) {
 };
 
 /**
- * Function handles misspelled family names or similarlly spelled names
+ * Function handles misspelled family names or similar spellings
  * and returns the correct spelling
  * @param name is the parsed family name from the API
  * @returns the correct spelling of the family name or returns
- * the original name if no match is found
+ * the original name if no match for known misspellings are found
  */
 const filterSimilarNameSpellings = function filterSimilarNameSpellings(name) {
   switch (name) {
@@ -154,51 +199,68 @@ const filterSimilarNameSpellings = function filterSimilarNameSpellings(name) {
  * function will validate that the family name is
  * valid (not Unknown, Empty, or None)
  * @param name is the parsed family name from the API
- * @returns the name if valid or returns null
+ * @returns the name if valid or returns "Unknown House"
  */
 const validateFamilyName = function validateFamilyName(name) {
   if (name === "" || name === "Unknown" || name === "Unkown" || name === "None")
-    return null;
+    return "Unknown House";
   return name;
 };
 
 /**
- * Seperates the family name from data
- * Sets family name and length of name to new data array
- * @param data array of charcter objects
- * @returns nameAndCount object containing familyName array
- * and count of name array
+ * Function will correct the spelling of family names and
+ * group unknown or empty family house names together
+ * @param data is the character data array
+ * @param familyNames is a temporary array to hold the family names
+ * No return object, the 'familyNames' array is passed by reference
+ */
+const cleanFamilyName = function filterNamesForEmptyOrMispelled(
+  data,
+  familyNames
+) {
+  data.map((character) => {
+    const familyName = validateFamilyName(
+      filterSimilarNameSpellings(
+        character.family.split("House").join("").trim()
+      )
+    );
+    familyNames.push(familyName);
+  });
+};
+
+/**
+ * Clean and seperates the object data by house family name,
+ * sorts the array of names alphabetically [a-zA-Z],
+ * combines the family names into a new array,
+ * and counts the number of times a family name occurs
+ * @param data array of charcter objects from API
+ * @returns an object containing the unique family names
+ * and count of each family name occurance
  */
 const getChartData = function setFamilyNameAndLengthOfName(data) {
-  const uniqueFamilyNames = [];
-  const familyNameLength = [];
-  let familyNameAndNameLength = {};
+  const familyNames = [];
+  const familyNamesOccurance = [];
+  let uniqueFamilyNames = [];
 
   if (data) {
-    // sort family names and push unique names to new array
-    data.map((character) => {
-      const familyName = validateFamilyName(
-        filterSimilarNameSpellings(
-          character.family.split("House").join("").trim()
-        )
-      );
-      if (familyName && !uniqueFamilyNames.includes(familyName)) {
-        uniqueFamilyNames.push(familyName);
-      }
-    });
-    // sort the array of family names alphabetically
-    sortAlpha(uniqueFamilyNames);
-    // push the length of each family name to new array
-    uniqueFamilyNames.map((name) => {
-      familyNameLength.push(name.length);
-    });
-    // join the family name and length of name to new array
-    familyNameAndNameLength = {
+    // clean and correct spelling of family names
+    cleanFamilyName(data, familyNames);
+    // sort the array of family names alphabetically [a-zA-Z]
+    sortAlpha(familyNames);
+    // combine the family names into a new array
+    uniqueFamilyNames = combineFamilyNames(familyNames);
+    // count the number of times a family name occurs and set to FamilyNamesOccurance array
+    countFamilyNameOccurance(
+      uniqueFamilyNames,
+      familyNames,
+      familyNamesOccurance
+    );
+    // return the object containing the family name and count of name occurance
+    return {
       familyName: uniqueFamilyNames,
-      count: familyNameLength,
+      count: familyNamesOccurance,
     };
   }
-  return familyNameAndNameLength;
 };
 
 /**
@@ -223,7 +285,6 @@ const fetchData = async function fetchCharactersAPI(url) {
  * renderChart()
  */
 window.addEventListener("load", async () => {
-  // url for the Thrones API
   const url = "https://thronesapi.com/api/v2/Characters";
   try {
     const data = await fetchData(url);
