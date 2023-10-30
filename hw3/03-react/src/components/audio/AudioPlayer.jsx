d89c8bf3e0cdc437edc8d2017ee1ab1f97c88850
Todd <thickam@outlook.com>
2023-10-29 21:02:40 -0700

    added source for audio file and player

--- hw3/03-react/src/components/audio/AudioPlayer.jsx
+++ hw3/03-react/src/components/audio/AudioPlayer.jsx
@@ -1,4 +1,7 @@
 /**
+ * Audio Player Component: add to navbar Layout.jsx to play audio
+ * across all pages
+ * Game of Thrones Audio File Source: https://archive.org/details/01MainTitle_201905
  * Audio Player Source: https://www.npmjs.com/package/react-h5-audio-player
  */
 import "./styles/audio.css"
